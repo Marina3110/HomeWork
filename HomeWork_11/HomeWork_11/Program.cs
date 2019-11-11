0853f143b458f8909589ce41eeb35db5e032502d
Marina3110 <53663239+Marina3110@users.noreply.github.com>
2019-11-11 21:13:40 +0300

    Add files via upload
    Home work #1 (task 1,2)

---
+++ HomeWork_11/HomeWork_11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_11
+{
+    class Program
+    {
+        static void Output(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размерность массива: ");
+            int N = int.Parse(Console.ReadLine());
+            int[] array = new int[N];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next()%100;
+            }
+            Output(array);
+
+            Console.WriteLine("\nСортировка по возрастанию:");
+            Array.Sort(array);
+            Output(array);
+
+            Console.WriteLine("\nСортировка по убыванию:");
+            Array.Reverse(array);
+            Output(array);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork_12/HomeWork_12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeWork_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер сторон треугольника: ");
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 1; j < N - i; j++)
+                    Console.Write(' ');
+                for (int j = N - 2 * i; j <= N; j++)
+                {
+                    Console.Write('*');
+                }
+                    
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
